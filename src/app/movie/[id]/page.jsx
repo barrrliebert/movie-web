e95a2b1ed23858380a7e820cc18aea55b97612c9
Akbar T. Ramadan <akbartolieb@gmail.com>
2023-10-25 21:48:01 +0700

    Update page.jsx

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -154,12 +154,6 @@ export default function MovieDetailPage() {
                 <p>{movieData.director}</p>
                 </div>
     
-                <div>
-                <p className="text-xl font-semibold">Featured song:</p>
-                <p>{movieData.featured_song}</p>
-                </div>
-                <div>
-    
                 <div className="flex justify-center gap-32   mt-10"></div>
             </div>
             </div>
