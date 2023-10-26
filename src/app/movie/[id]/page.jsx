84feafe0055e4ea5f567cd4f606a237c8ac06686
Akbar T. Ramadan <akbartolieb@gmail.com>
2023-10-26 19:47:54 +0700

    Add files via upload

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -5,164 +5,150 @@ import { useState } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function MovieDetailPage() {
-  const params = useParams();
-  const movieId = params.id;
-  const movieData = useGetDetailMovie(movieId);
+const params = useParams();
+const movieId = params.id;
+const movieData = useGetDetailMovie(movieId);
 
-  const [sortBy, setSortBy] = useState("all");
+const [sortBy, setSortBy] = useState("all");
 
-  if (!movieData) {
-    return <p>Loading...</p>;
-  }
-
-  return (
-    <main className="container mx-auto mt-10 relative">
-        
-        <div className="relative w-full px-[5rem] lg:px-[10rem] py-[5rem] h-max text-left text-17xl text-white font-roboto bg-cover " style={{ backgroundImage: 'url(https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-04.jpg)' }}>
-
-                <div className="flex flex-col items-start justify-center">
-                <div className=" text-[30px] sm:text-[50px] lg:text-[70px] font-black w-[70%]">
-                    {movieData.title}
-                </div>
-
-                <div className="flex items-center justify-center space-x-5 mt-[3rem] lg:mt-[8rem]">
-                    <div className="flex">
-                        <svg className="w-7 sm:w-14"
-                            alt=""
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 49 38"
-                            fill="#EBCD00">
-                            <path d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
-                        </svg>
-                        <svg className="w-7 sm:w-14"
-                            alt=""
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 49 38"
-                            fill="#EBCD00">
-                            <path d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
-                        </svg>
-                        <svg className="w-7 sm:w-14"
-                            alt=""
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 49 38"
-                            fill="#EBCD00">
-                            <path d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
-                        </svg>
-                        <svg className="w-7 sm:w-14"
-                            alt=""
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 49 38"
-                            fill="#EBCD00">
-                            <path d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
-                        </svg>
-                        <svg className="w-7 sm:w-14"
-                            alt=""
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 49 38"
-                            fill="#E4E4E4">
-                            <path d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
-                        </svg>
-                    </div>
-
-                    <h5 className=" sm:text-normal text-sm font-medium mt-2">
-                        2200 reviews
-                    </h5>
-                </div>
-
-                <p className="m-0 break-words w-20rem sm:w-[40rem] mt-5 sm:text-normal text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-
-                <div className="flex sm:flex-row flex-col space-y-3 sm:space-y-0 sm:space-x-5 mt-[5rem]">
-                    <button className="sm:py-3 py-2 sm:px-10 px-5 bg-red-500 w-max text-white text-lg sm:text-2xl font-black rounded-md hover:bg-red-700">
-                        Watch Trailer
-                    </button>
-                    <button className="sm:py-3 py-2 sm:px-10 px-5 bg-transparent w-max text-white text-lg sm:text-2xl font-bold rounded-md border border-white hover:bg-gray-500 hover:text-white">
-                        Add to Watchlist
-                    </button>
-                    
-                </div>
-            </div>
-
-    </div>
-            <div className="flex flex-col justify-between max-h-screen p-10">
-                {/* Content */}
-                <div>
-                <div className="flex flex-col lg:flex-row items-center">
-                    <div className="flex space-x-2">
-                    <button
-                        className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "all" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-                        onClick={() => setSortBy("all")}
-                    >
-                        Overview
-                    </button>
-                    <button
-                        className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "popularity.desc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-                        onClick={() => setSortBy("popularity.desc")}
-                    >
-                        Characters
-                    </button>
-                    <button
-                        className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "popularity.sc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-                        onClick={() => setSortBy("popularity.sc")}
-                    >
-                        Review
-                    </button>
-                    </div>
-                </div>
-    
-                <div className="flex justify-center gap-32   mt-10"></div>
-            
-    
-                <div className="flex flex-row items-center">
-                    <h1 className="text-2xl lg:text-5xl font-bold mb-4">Synopsis</h1>
-                    <div className="border-t border-gray-500 flex-grow ml-4"></div>
-                </div>
-                <h1 className="text-2xl lg:text-3xl font-bold mb-4">{movieData.title}</h1>
-                <p className="lg:text-lg">Overview: {movieData.overview}</p>
-    
-                <div className="flex justify-center gap-32 mt-10"></div>
-    
-                <div className="flex flex-row items-center">
-                    <h1 className="text-2xl lg:text-5xl font-bold mb-4">Movie Info</h1>
-                    <div className="border-t border-gray-500 flex-grow ml-4"></div>
-                </div>
-                {/* Display Movie Info */}
-                <div className="mt-4">
-                <div>
-                <p className="text-xl font-semibold">Release date:</p>
-                <p>{movieData.release_date}</p>
-                </div>
-    
-                <div>
-                <p className="text-xl font-semibold">Budget:</p>
-                <p>{movieData.budget} USD</p>
-                </div>
-    
-                <div>
-                <p className="text-xl font-semibold">Director:</p>
-                <p>{movieData.director}</p>
-                </div>
-    
-                <div>
-                <p className="text-xl font-semibold">Featured song:</p>
-                <p>{movieData.featured_song}</p>
-                </div>
-                <div>
-                <p className="text-xl font-semibold">Release date:</p>
-                <p>{movieData.release_date}</p>
-                </div>
-    
-    
-        
-    
-                
-                <div className="flex justify-center gap-32   mt-10"></div>
-            </div>
-            </div>
-            </div>
-
-    </main>
-  );
+if (!movieData) {
+return <p>Loading...</p>;
 }
 
+return (
+<main className="container mx-auto mt-10 relative">
+
+    <div className="relative w-full px-[5rem] lg:px-[10rem] py-[5rem] h-max text-left text-17xl text-white font-roboto bg-cover "
+        style={{ backgroundImage: 'url(https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-04.jpg)' }}>
+
+        <div className="flex flex-col items-start justify-center">
+            <div className=" text-[30px] sm:text-[50px] lg:text-[70px] font-black w-[70%]">
+                {movieData.title}
+            </div>
+
+            <div className="flex items-center justify-center space-x-5 mt-[3rem] lg:mt-[8rem]">
+                <div className="flex">
+                    <svg className="w-7 sm:w-14" alt="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49 38"
+                        fill="#EBCD00">
+                        <path
+                            d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
+                    </svg>
+                    <svg className="w-7 sm:w-14" alt="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49 38"
+                        fill="#EBCD00">
+                        <path
+                            d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
+                    </svg>
+                    <svg className="w-7 sm:w-14" alt="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49 38"
+                        fill="#EBCD00">
+                        <path
+                            d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
+                    </svg>
+                    <svg className="w-7 sm:w-14" alt="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49 38"
+                        fill="#EBCD00">
+                        <path
+                            d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
+                    </svg>
+                    <svg className="w-7 sm:w-14" alt="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49 38"
+                        fill="#E4E4E4">
+                        <path
+                            d="M24.5 0L30.2251 14.5106H48.7519L33.7634 23.4787L39.4885 37.9894L24.5 29.0213L9.51148 37.9894L15.2366 23.4787L0.248058 14.5106H18.7749L24.5 0Z" />
+                    </svg>
+                </div>
+
+                <h5 className=" sm:text-normal text-sm font-medium mt-2">
+                    2200 reviews
+                </h5>
+            </div>
+
+            <p className="m-0 break-words w-20rem sm:w-[40rem] mt-5 sm:text-normal text-sm">
+                {movieData.overview}
+            </p>
+
+            <div className="flex sm:flex-row flex-col space-y-3 sm:space-y-0 sm:space-x-5 mt-[5rem]">
+                <button
+                    className="sm:py-3 py-2 sm:px-10 px-5 bg-red-500 w-max text-white text-lg sm:text-2xl font-black rounded-md hover:bg-red-700">
+                    Watch Trailer
+                </button>
+                <button
+                    className="sm:py-3 py-2 sm:px-10 px-5 bg-transparent w-max text-white text-lg sm:text-2xl font-bold rounded-md border border-white hover:bg-gray-500 hover:text-white">
+                    Add to Watchlist
+                </button>
+
+            </div>
+        </div>
+
+    </div>
+    <div className="flex flex-col justify-between max-h-screen p-10">
+        <div>
+            <div className="flex flex-col lg:flex-row items-center">
+                <div className="flex space-x-2">
+                    <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy==="all"
+                        ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`} onClick={()=> setSortBy("all")}
+                        >
+                        Overview
+                    </button>
+                    <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white
+                        ${sortBy==="popularity.desc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`}
+                        onClick={()=> setSortBy("popularity.desc")}
+                        >
+                        Characters
+                    </button>
+                    <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white
+                        ${sortBy==="popularity.sc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`}
+                        onClick={()=> setSortBy("popularity.sc")}
+                        >
+                        Review
+                    </button>
+                </div>
+            </div>
+
+            <div className="flex justify-center gap-32   mt-10"></div>
+
+
+            <div className="flex flex-row items-center">
+                <h1 className="text-2xl lg:text-5xl font-bold mb-4">Synopsis</h1>
+                <div className="border-t border-gray-500 flex-grow ml-4"></div>
+            </div>
+            <h1 className="text-2xl lg:text-3xl font-bold mb-4">{movieData.title}</h1>
+            <p className="lg:text-lg">Overview: {movieData.overview}</p>
+
+            <div className="flex justify-center gap-32 mt-10"></div>
+
+            <div className="flex flex-row items-center">
+                <h1 className="text-2xl lg:text-5xl font-bold mb-4">Movie Info</h1>
+                <div className="border-t border-gray-500 flex-grow ml-4"></div>
+            </div>
+            {/* Display Movie Info */}
+            <div className="mt-4">
+                <div>
+                    <p className="text-xl font-semibold">Release date:</p>
+                    <p>{movieData.release_date}</p>
+                </div>
+
+                <div>
+                    <p className="text-xl font-semibold">Budget:</p>
+                    <p>{movieData.budget} USD</p>
+                </div>
+
+                <div>
+                    <p className="text-xl font-semibold">Director:</p>
+                    <p>{movieData.director}</p>
+                </div>
+
+                <div>
+                    <p className="text-xl font-semibold">Featured song:</p>
+                    <p>{movieData.featured_song}</p>
+                </div>
+                <div>
+                    <p className="text-xl font-semibold">Release date:</p>
+                    <p>{movieData.release_date}</p>
+                </div>
+
+                <div className="flex justify-center gap-32   mt-10"></div>
+            </div>
+        </div>
+    </div>
+
+</main>
+);
+}
--- src/app/sections/ListMovie.jsx
+++ src/app/sections/ListMovie.jsx
@@ -11,151 +11,155 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 const maxPages = 6;
 
 export default function ListMovie() {
-    const [sortBy, setSortBy] = useState('popularity.desc')
-    const params = useSearchParams()
-    const data = useGetMovies(sortBy, params.get('page'))
-    const [page, setPage] = useState(1);
+const [sortBy, setSortBy] = useState('popularity.desc')
+const params = useSearchParams()
+const data = useGetMovies(sortBy, params.get('page'))
+const [page, setPage] = useState(1);
 
-    useEffect(() => {
-        const pageParam = parseInt(params.get('page')) || 1;
-        setPage(pageParam);
-      }, [params.get('page')]);
+useEffect(() => {
+const pageParam = parseInt(params.get('page')) || 1;
+setPage(pageParam);
+}, [params.get('page')]);
 
-      const goToPreviousPage = () => {
-        if (page > 1) {
-          setPage(page - 1);
-        }
-      };
-    
-      const goToNextPage = () => {
-        if (page < maxPages) {
-          setPage(page + 1);
-        }
-      };
+const goToPreviousPage = () => {
+if (page > 1) {
+setPage(page - 1);
+}
+};
 
-    return (
-        <>
-       <Carousel
-  showArrows={true}
-  showStatus={false}
-  showThumbs={false}
-  infiniteLoop={true}
-  selectedItem={1} // Adjust the initial selected item as needed
-  centerMode={true}
-  centerSlidePercentage={100} // Set to 100 to center the selected item
-  className="max-w-screen-2xl mx-auto" // Increase the maximum width
-  style={{ width: "80%", margin: "0 auto" }} // Set the width and center the carousel
->
-  {data?.results?.map((item, index) => (
-    <Link href={`/movie/${item.id}`} key={item.id}>
-      <div className="bg-white p-4 shadow-md rounded-lg" style={{ width: "100%", height: "auto" }}>
-        <div className="banner-wrapper">
-          <img
-            src="https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-04.jpg"
-            alt={item.title}
-            className="w-full h-auto object-cover"
-          />
+const goToNextPage = () => {
+if (page < maxPages) { setPage(page + 1); } }; return ( <>
+  {data?.results && data.results.length >= 3 && (
+  <Carousel showArrows={true} showStatus={false} showThumbs={false} infiniteLoop={true} centerMode={true}
+    centerSlidePercentage={100} className="max-w-screen-2xl mx-auto" style={{ width: "80%", margin: "0 auto" }}>
+    {data?.results?.reduce((slideGroups, item, index) => {
+    if (index % 3 === 0) {
+    slideGroups.push([]);
+    }
+    slideGroups[slideGroups.length - 1].push(item);
+    return slideGroups;
+    }, []).map((slide, slideIndex) => (
+    <div key={slideIndex} className="max-w-screen-2xl mx-auto">
+      <div className="banner-container" style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gap: "16px" }}>
+        {slide.map((subItem, subIndex) => (
+        <Link href={`/movie/${subItem.id}`} key={subItem.id}>
+        <div className="bg-white p-4 shadow-md rounded-lg">
+          <div className="banner-wrapper">
+            <img src={`https://image.tmdb.org/t/p/w500${subItem.poster_path}`} alt={subItem.title}
+              className="w-full h-auto object-cover" />
+          </div>
         </div>
+        </Link>
+        ))}
       </div>
-    </Link>
-  ))}
-</Carousel>
-
-        <h1 className="text-2xl font-bold mb-4 px-5">Browse by category</h1>
-     <div className="w-full flex justify-between items-start space-x-3 overflow-x-scroll scrollbar">
-     
-  <div className="flex space-x-2">
-  <button
-      className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "all" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-      onClick={() => setSortBy("all")}
-    >
-      All
-    </button>
-    <button
-      className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "popularity.desc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-      onClick={() => setSortBy("popularity.desc")}
-    >
-      Anime
-    </button>
-    <button
-      className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "popularity.asc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-      onClick={() => setSortBy("popularity.asc")}
-    >
-      Action
-    </button>
-    <button
-      className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "revenue.asc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-      onClick={() => setSortBy("revenue.asc")}
-    >
-      Adventure
-    </button>
-    <button
-      className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "revenue.desc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-      onClick={() => setSortBy("revenue.desc")}
-    >
-      Science fiction
-    </button>
-    <button
-      className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy === "revenue.sc" ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}
-      onClick={() => setSortBy("revenue.sc")}
-    >
-      Comedy
-    </button>
-  </div>
-
-</div>
-<div className="flex items-center justify-center mt-10">
-  <div className="grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-5">
-    {data?.results?.map((item, index) => (
-      <Link href={`/movie/${item.id}`} key={item.id}>
-        <Card image={item.poster_path} overview={item.overview} id={item.id} popularity={item.popularity} title={item.title} />
-      </Link>
-    ))}
-  </div>
-</div>
-<div className="flex justify-center mt-10">
-  <ul className="flex items-center gap-4 sm:gap-28">
-  <li onClick={goToPreviousPage}>
-      <BsArrowLeft size={50} />
-    </li>
-    <li>
-      <Link href="/?page=1">
-        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 hover:transform hover:scale-105 ${page === 1 ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}>1</span>
-      </Link>
-    </li>
-    <li>
-      <Link href="/?page=2">
-        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 ${page === 2 ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}>2</span>
-      </Link>
-    </li>
-    <li>
-      <Link href="/?page=3">
-        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 ${page === 3 ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}>3</span>
-      </Link>
-    </li>
-    <li>
-      <Link href="/?page=4">
-        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 ${page === 4 ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}>4</span>
-      </Link>
-    </li>
-    <li>
-      <Link href="/?page=5">
-        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 ${page === 5 ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}>5</span>
-      </Link>
-    </li>
-    <li>
-      <Link href="/?page=6">
-        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 hover:transform hover:scale-105 ${page === 6 ? 'bg-red-500 text-white' : 'bg-white text-gray-500'}`}>6</span>
-      </Link>
-    </li>
-    <li onClick={goToNextPage}>
-      <BsArrowRight size={50} />
-    </li>
-  </ul>
-</div>
-
-    <div className="flex justify-center gap-32   mt-10">
     </div>
-    </>
-  );
+    ))}
+  </Carousel>
+  )}
+
+
+
+  <h1 className="text-2xl font-bold mb-4 px-5">Browse by category</h1>
+  <div className="w-full flex justify-between items-start space-x-3 overflow-x-scroll scrollbar">
+
+    <div className="flex space-x-2">
+      <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy==="all"
+        ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`} onClick={()=> setSortBy("all")}
+        >
+        All
+      </button>
+      <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy==="popularity.desc"
+        ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`} onClick={()=> setSortBy("popularity.desc")}
+        >
+        Anime
+      </button>
+      <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy==="popularity.asc"
+        ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`} onClick={()=> setSortBy("popularity.asc")}
+        >
+        Action
+      </button>
+      <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy==="revenue.asc"
+        ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`} onClick={()=> setSortBy("revenue.asc")}
+        >
+        Adventure
+      </button>
+      <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy==="revenue.desc"
+        ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`} onClick={()=> setSortBy("revenue.desc")}
+        >
+        Science fiction
+      </button>
+      <button className={`px-6 py-2 rounded-full hover:bg-red-500 hover:text-white ${sortBy==="revenue.sc"
+        ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`} onClick={()=> setSortBy("revenue.sc")}
+        >
+        Comedy
+      </button>
+    </div>
+
+  </div>
+  <div className="flex items-center justify-center mt-10">
+    <div className="grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-5">
+      {data?.results?.map((item, index) => (
+      <Link href={`/movie/${item.id}`} key={item.id}>
+      <Card image={item.poster_path} overview={item.overview} id={item.id} popularity={item.popularity}
+        title={item.title} />
+      </Link>
+      ))}
+    </div>
+  </div>
+  <div className="flex justify-center mt-10">
+    <ul className="flex items-center gap-4 sm:gap-28">
+      <li>
+        <Link href={`/?page=${page - 1}`}>
+        <BsArrowLeft size={50} />
+        </Link>
+      </li>
+
+      <li>
+        <Link href="/?page=1">
+        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 hover:transform hover:scale-105
+          ${page===1 ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`}>1</span>
+        </Link>
+      </li>
+      <li>
+        <Link href="/?page=2">
+        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 ${page===2 ? 'bg-red-500 text-white'
+          : 'bg-white text-gray-500' }`}>2</span>
+        </Link>
+      </li>
+      <li>
+        <Link href="/?page=3">
+        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 ${page===3 ? 'bg-red-500 text-white'
+          : 'bg-white text-gray-500' }`}>3</span>
+        </Link>
+      </li>
+      <li>
+        <Link href="/?page=4">
+        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 ${page===4 ? 'bg-red-500 text-white'
+          : 'bg-white text-gray-500' }`}>4</span>
+        </Link>
+      </li>
+      <li>
+        <Link href="/?page=5">
+        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 ${page===5 ? 'bg-red-500 text-white'
+          : 'bg-white text-gray-500' }`}>5</span>
+        </Link>
+      </li>
+      <li>
+        <Link href="/?page=6">
+        <span className={`text-2xl sm:text-4xl hover:text-white hover:bg-red-500 hover:transform hover:scale-105
+          ${page===6 ? 'bg-red-500 text-white' : 'bg-white text-gray-500' }`}>6</span>
+        </Link>
+      </li>
+      <li>
+        <Link href={`/?page=${page + 1}`}>
+        <BsArrowRight size={50} />
+        </Link>
+      </li>
+    </ul>
+  </div>
+
+  <div className="flex justify-center gap-32   mt-10">
+  </div>
+</>
+);
 };
